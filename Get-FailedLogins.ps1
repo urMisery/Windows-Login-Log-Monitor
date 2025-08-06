969f8b187a9cb6090f455860d20be184c79dc92d
urMisery <urmiseryukr@gmail.com>
2025-08-06 20:18:43 +0200

    Initial commit: PowerShell script for login monitoring

---
+++ Get-FailedLogins.ps1
@@ -0,0 +1,35 @@
+#Get-FailedLogins.ps1
+#Анализ неудачных и успешных входов в систему из Security журнала
+
+$timeFrame = (Get-Date).AddHours(-24) # Last 24 hours
+
+#Getting Failed Logins (4625)
+$failedLogons = Get-WinEvent -FilterHashtable @{LogName='Security'; Id = 4625; StartTime=$timeFrame}|
+	ForEach-Object{
+		$xml = [xml]$_.ToXml()
+		[PSCustomObject]@{
+			TimeCreated = $_.TimeCreated
+			Account = $xml.Event.EventData.Data[5].'#text'
+			IPAddress = $xml.Event.EventData.Data[18].'#text'
+			FailureReason = $xml.Event.EventData.Data[11].'#text'
+		}
+	}
+
+#Getting success logons (4624)
+$successLogons = Get-WinEvent -FilterHashtable @{LogName='Security'; Id=4624; StartTime=$timeFrame}|
+	ForEach-Object{
+		$xml = [xml]$_.ToXml()
+		[PSCustomObject]@{
+			TimeCreated = $_.TimeCreated
+			Account = $xml.Event.EventData.Data[5].'#text'
+			IPAdress = $xml.Event.EventData.Data[18].'#text'
+			LogonType = $xml.Event.EventData.Data[8].'#text'
+		}
+	}
+
+#Output
+Write-Host "`n===== Failed Logons (4625) =====`n"
+$failedLogons | Format-Table
+
+Write-Host "`n===== Successful Logons (4624) =====`n"
+$successLogons | Format-Table
